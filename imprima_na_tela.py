02e0b894091a5674901fec5737960065bd767e95
Gabriel Ponzoni <gabrielponzoni1234@gmail.com>
2023-03-23 11:06:58 -0300

    Exercicio 1
    (v1) Edit: Enviando atividade para o github

---
+++ imprima_na_tela.py
@@ -0,0 +1,6 @@
+#1. Imprima o seu primeiro nome usando texto fixo e em seguida usando uma variável
+
+print('Gabriel')
+print('O de cima foi com texto fixo!')
+nome = Gabriel
+print('Meu nome numa variável chamada "nome":',nome)
