23f856135d2c5750d1220ccdb95c0828cd792bc9
Gabriel Ponzoni <gabrielponzoni1234@gmail.com>
2023-03-24 10:39:44 -0300

    Atividade 6
    2 variaveis para a maquina

---
+++ imprima_entrada_data_mes.py
@@ -0,0 +1,5 @@
+#6. Solicite ao usuário o dia e mês do seu nascimento e imprima no terminal o dia e mês 
+#informados
+
+data, mes = int(input("Informe o dia e o mês do seu nascimento: ")),int(input())
+print("Sua data de aniversário é {:02d}/{:02d}".format(data,mes))
