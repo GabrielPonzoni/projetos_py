c4eea7912af8fdc6ba739de879431131056981b3
Gabriel Ponzoni <gabrielponzoni1234@gmail.com>
2023-03-24 10:36:51 -0300

    Atividade 5
    comando de input para a máquina

---
+++ imprima_entrada_data.py
@@ -0,0 +1,7 @@
+#5. Solicite ao usuário o dia do seu nascimento e imprima no terminal o dia informado
+#del print <- utilizei para limpar a saida do print estava dando erro de 'str' not [...]
+# Solicita ao usuário o dia do seu nascimento
+dia_nascimento = input("Digite o dia do seu nascimento: ")
+
+# Imprime o dia informado
+print("Dia de nascimento:", dia_nascimento)
