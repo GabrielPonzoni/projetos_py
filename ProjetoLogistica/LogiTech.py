a3bfa7cd348a64ddcecd615356a2e2f133210636
Gabriel Ponzoni <gabrielponzoni1234@gmail.com>
2023-04-05 11:19:05 -0300

    Tentando melhorar a formatação

--- ProjetoLogistica/LogiTech.py
+++ ProjetoLogistica/LogiTech.py
@@ -1,20 +1,22 @@
 def cabecalho():
-  print('-=-' * 20)
-  print('||          LogiTech Especialistas em lojistica!          ||')
-  print('-=-' * 20)
+  
+    print('-=-' * 20)
+    print('||          LogiTech Especialistas em lojistica!          ||')
+    print('-=-' * 20)
 
 def menu():
-  print('||                        MENU                            ||')
-  print('-=-' * 20)
-  print('||Informe um número para acessar o item do menu, ou deixe || \n||o espaço em branco pressionando ENTER para sair.        ||')
-  print('||                                                        ||')
-  print('||() Registrar venda',' '* 37,'||')                                  
-  print('||() Repor estoque')
-  print('||() Mostrar estoque')
-  print('||() Mostrar compras')
-  print('||() Maior compra')
-  print('||() Sair')
-  print('-=-' * 20)
+  
+    print('||                        MENU                            ||')
+    print('-=-' * 20)
+    print('||Informe um número para acessar o item do menu, ou deixe || \n||o espaço em branco pressionando ENTER para sair.        ||')
+    print('||                                                        ||')
+    print('||() Registrar venda',' '* 37,'||')                                  
+    print('||() Repor estoque')
+    print('||() Mostrar estoque')
+    print('||() Mostrar compras')
+    print('||() Maior compra')
+    print('||() Sair')
+    print('-=-' * 20)
 
 cabecalho()
 menu()
