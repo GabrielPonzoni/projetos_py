0863a6ba71d00268a0b84761487ce0218b8b218e
Gabriel Ponzoni <gabrielponzoni1234@gmail.com>
2023-04-05 11:45:45 -0300

    Instanciei as funções do menu

--- ProjetoLogistica/LogiTech.py
+++ ProjetoLogistica/LogiTech.py
@@ -1,9 +1,9 @@
 
-def main():
+def main(): 
     
 	cabecalho() #1ºprimeiro passo foi criar o cabecalho que irá ficar fixo no topo da tela
 	menu() #2ºoutro passo importante é o menu 
-	resposta = int(input()) #3ºrecebendo valores	
+	resposta = int(input()) #3ºrecebendo valores
 # end def-main
 
 def cabecalho():
@@ -19,13 +19,52 @@ def menu():
     print('-=-' * 20)
     print('||Informe um número para acessar o item do menu, ou deixe || \n||o espaço em branco pressionando ENTER para sair.        ||')
     print('||                                                        ||') 
-    print('||() Registrar venda', ' ' * 37, '||')
-    print('||() Repor estoque')
-    print('||() Mostrar estoque')
-    print('||() Mostrar compras')
-    print('||() Maior compra')
-    print('||() Sair')
+    print('||(1) Registrar venda', ' ' * 35, '||')
+    print('||(2) Repor estoque')
+    print('||(3) Mostrar estoque')
+    print('||(4) Mostrar compras')
+    print('||(5) Maior compra')
+    print('||(0) Sair')
     print('-=-' * 20)
+
+#funções do 1 ao 5
+def registrar_venda():
+    """
+    Purpose: 
+    """ 
+# end def
+
+def repor_estoque():
+    """
+    Purpose: 
+    """   
+# end def
+
+def mostrar_estoque():
+    """
+    Purpose: 
+    """
+# end def
+
+def mostrar_compras():
+    """
+    Purpose: 
+    """
+# end def
+
+def maior_compra():
+    """
+    Purpose: 
+    """
+# end def
+
+def sair():
+    """
+    Purpose: 
+    """
+# end def
+
+#fim das funções do menu
 
 if __name__ == "__main__": #1.1º criei a main
 	main()
