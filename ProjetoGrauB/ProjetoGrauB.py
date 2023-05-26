e7fd67bbd762be52337bc1dd11386191f9d94527
Gabriel "Syll3x" Ponzoni <109359526+GabrielPonzoni@users.noreply.github.com>
2023-05-26 11:19:48 -0300

    v1.1 - sixPack
    +Função que mostra o cabeçalho;

--- ProjetoGrauB/ProjetoGrauB.py
+++ ProjetoGrauB/ProjetoGrauB.py
@@ -1,9 +1,21 @@
 def main():
-
     """
     Propósito: Main do projeto  
     """
+    cabecalho()
+
     # end def
+
+def cabecalho():
+    """
+    Purpose: Função que mostra o cabecalho
+    """
+    
+    nome_empresa = "Six Pack Logistica " + chr(169)
+    print('='*60)
+    print(f'||{nome_empresa:^56}||')
+    print('='*60)
+# end def
 
 if __name__ == "__main__":
     main()
