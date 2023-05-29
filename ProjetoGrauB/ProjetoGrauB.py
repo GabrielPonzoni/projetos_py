67902d15575099160121f8adf3107c3468c4eead
Gabriel "Syll3x" Ponzoni <109359526+GabrielPonzoni@users.noreply.github.com>
2023-05-29 15:07:02 -0300

    v.1.5
    -Troquei a a lista por uma variável simples para armazenar o preço por km rodado

--- ProjetoGrauB/ProjetoGrauB.py
+++ ProjetoGrauB/ProjetoGrauB.py
@@ -1,7 +1,7 @@
 import os  # biblioteca que possui comandos do sistema operacional
 from time import sleep  # biblioteca que possui um sys PAUSE por tanto tempo
 
-custo_km_rodado_armazenado = []
+custo_km_rodado = 0
 fechar_programa = False
 
 
@@ -9,6 +9,7 @@ def main():
     '''
     Propósito: Main do projeto  
     '''
+    global custo_km_rodado
     limpa_tela()
 
     while not fechar_programa:
@@ -16,6 +17,8 @@ def main():
         cabecalho()
         menu()
         opcoes_menu()
+        #só realiza essa parte do código quando escolho 5 no menu
+        sleep(1)
 
     # end while
 # end def
@@ -54,12 +57,6 @@ def cabecalho_kmrodado():
     print('||', ' '*54, '||')
     print(f'||', '-_'*27, '||')
     print('||', ' '*54, '||')
-    print('||', '1 - Custo por Km rodado;', ' '*29, '||')
-    print('||', '2 - Consultar trecho;', ' '*32, '||')
-    print('||', '3 - Melhor rota;', ' '*37, '||')
-    print('||', '4 - Rota completa;', ' '*35, '||')
-    print('||', '5 - Sair;', ' '*44, '||')
-    print('||', ' '*54, '||')
     print('='*60)
 # end def
 
@@ -86,10 +83,10 @@ def sair():
 # end def
 
 def opcoes_menu():
-
+    global custo_km_rodado #declaração da variável global dentro da função que a altera
     escolha = input("Seleciona a opção desejada: ")
     if escolha == "1":
-        add_cust_km(custo_km_rodado_armazenado)
+       custo_km_rodado = add_cust_km(custo_km_rodado)
     elif escolha == "2":
         print('Opção 2')
     elif escolha == "3":
@@ -105,45 +102,24 @@ def opcoes_menu():
 # end def
 
 
-def add_cust_km(custo_km_rodado_armazenado):
+def add_cust_km(custo_km_rodado):
     limpa_tela()
     cabecalho()
     cabecalho_kmrodado()
-    if len(custo_km_rodado_armazenado) > 0:
-        custo_km_rodado_armazenado.pop()
-        while True:
-            try:
-                custo_km_rodado = float(
-                    input("Digite o custo por Km rodado: "))
-                if custo_km_rodado <= 0:
-                    # Se o valor inserido for negativo, levanta um ValueError para acionar o bloco except
-                    raise ValueError
-                break  # Se o valor informado for um número, sai do loop
-            except ValueError: 
-                # Se o usuário digitar algo que não éum número, exibe uma mensagem de erro e continua o loop
-                print(">>> Valor inválido! Digite um valor em reias positivo!")
-        custo_km_rodado_armazenado.append(custo_km_rodado)
-        print(custo_km_rodado_armazenado)
-        input()
-        main()
-    # end if
-
-    if len(custo_km_rodado_armazenado) <= 0:
-        while True:
-            try:
-                custo_km_rodado = float(
-                    input("Digite o custo por Km rodado: "))
-                if custo_km_rodado <= 0:
-                    # Se o valor inserido for negativo, levanta um ValueError para acionar o bloco except
-                    raise ValueError
-                break  # Se o valor informado for um número, sai do loop
-            except ValueError:
-                # Se o usuário digitar algo que não é um número, exibe uma mensagem de erro e continua o loop
-                print(">>> Valor inválido! Digite um valor em reias positivo!")
-        custo_km_rodado_armazenado.append(custo_km_rodado)
-        print(custo_km_rodado_armazenado)
-        input()
-        main()
+    while True:
+        try:
+            custo_km_rodado = float(input("Digite o custo por Km rodado: "))
+            if custo_km_rodado <= 0:
+                # Se o valor inserido for negativo, levanta um ValueError para acionar o bloco except
+                raise ValueError
+            break  # Se o valor informado for um número, sai do loop
+        except ValueError: 
+            # Se o usuário digitar algo que não éum número, exibe uma mensagem de erro e continua o loop
+            print(">>> Valor inválido! Digite um valor em reias positivo!")
+    print(custo_km_rodado)
+    sleep(2)
+    
+    return custo_km_rodado
     # end if
 
 
@@ -151,8 +127,7 @@ def limpa_tela():
     """
     Purpose: limpar tela
     """
-    os.system('cls' if os.name ==
-              'nt' else 'clear')  # vai por cls se sistema operacional for IOS ou se for Windows usa clear
+    os.system('cls' if os.name == 'nt' else 'clear')  # vai por cls se sistema operacional for IOS ou se for Windows usa clear
 
 
 # end def
